6d55999a49635ac7a62fc09a8c11a5aee74824d9
Vlad Palmanov <palmanov.work@gmail.com>
2022-11-01 02:38:35 +0300

    fix bug

--- include/listnode.h
+++ include/listnode.h
@@ -15,8 +15,8 @@ struct ListNode
   T val;
   ListNode<T>* next;
   ListNode() : val(0), next(nullptr) {}
-  explicit ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode<T>* next) : val(x), next(next) {}
+  explicit ListNode(T x) : val(x), next(nullptr) {}
+  ListNode(T x, ListNode<T>* next) : val(x), next(next) {}
   ~ListNode() {
     if (next)
       delete next;
